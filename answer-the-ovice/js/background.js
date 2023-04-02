9b54bc4dfa8a6d7e13bf74eb052a130053cd7ebe
iwasaki <picto@outlook.com>
2023-04-02 18:44:49 +0900

    リファクタリング

--- answer-the-ovice/js/background.js
+++ answer-the-ovice/js/background.js
@@ -1,47 +1,103 @@
-chrome.action.onClicked.addListener(async () => {
-  // 全てのタブを取得する
-  chrome.tabs.query({}, function (tabs) {
-    // 全てのタブについて繰り返す
-    for (const tab of tabs) {
-      // タブのURLが https://*** */ の場合
-      if (tab.url.startsWith('https://***')) {
-        // タブにメッセージを送信してボタンを押す
-        chrome.tabs.sendMessage(tab.id, { message: 'click_button' }, (response) => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log('Message sent successfully');
-          }
-        });
-        
-      }
+let globalIconState = {
+  isOn: false,
+  isDisabled: false,
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    requestCheckButton().then();
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    if (!tab.url.startsWith("https://***.ovice.in/")) {
+      updateExtensionIconImpl(activeInfo.tabId, globalIconState);
     }
   });
 });
 
+chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+  requestCheckButton();
+});
+
+chrome.action.onClicked.addListener(async () => {
+  sendClickButtonMessage();
+});
   
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'update_icon') {
     updateExtensionIcon(request.isOn, request.disabled);
   }
 });
-  
-chrome.tabs.onActivated.addListener(() => {
-  updateExtensionIcon(false, false);
-});
-  
+
+function sendCheckButtonMessage() {
+  sendMessageToOviceTab({ message: "check_button" });
+}
+
+function sendClickButtonMessage() {
+  sendMessageToOviceTab({ message: "click_button" });
+}
+
+function sendMessageToOviceTab(message) {
+  chrome.tabs.query({}, function (tabs) {
+    for (const tab of tabs) {
+      if (tab.url.startsWith('https://***.ovice.in/')) {
+        chrome.tabs.sendMessage(tab.id, message, (response) => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log("Message sent successfully");
+          }
+        });
+        return;
+      }
+    }
+  });
+}
+
+async function requestCheckButton() {
+  const hasOvice = await hasOviceTab();
+  if (hasOvice) {
+    sendCheckButtonMessage();
+  } else {
+    updateExtensionIcon(false, true);
+  }
+}
+
 function updateExtensionIcon(isOn, isDisabled) {
+  globalIconState.isOn = isOn;
+  globalIconState.isDisabled = isDisabled;
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const activeTab = tabs[0];
     if (activeTab) {
-      const iconPath = isDisabled
-        ? '../image/icon_disable.png'
-        : isOn
-        ? '../image/icon_on.png'
-        : '../image/icon_off.png';
-  
-      chrome.action.setIcon({ tabId: activeTab.id, path: iconPath });
+      updateExtensionIconImpl(activeTab.id, globalIconState)
     }
+  });
+}
+
+function updateExtensionIconImpl(tabId, iconState) {
+  const iconPath = iconState.isDisabled
+    ? "../image/icon_disable.png"
+    : iconState.isOn
+    ? "../image/icon_on.png"
+    : "../image/icon_off.png";
+
+  chrome.action.setIcon({ tabId: tabId, path: iconPath });
+}
+
+async function hasOviceTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({}, (tabs) => {
+      for (const tab of tabs) {
+        if (tab.url.startsWith("https://***.ovice.in/")) {
+          resolve(true);
+          return;
+        }
+      }
+      resolve(false);
+    });
   });
 }
   
--- answer-the-ovice/js/content.js
+++ answer-the-ovice/js/content.js
@@ -1,61 +1,78 @@
+let observeButtonRetryCount = 0;
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'click_button') {
     clickButton();
     sendResponse({});
+  } else if (request.message === 'check_button') {
+    checkButtonStatus();
+    sendResponse({});
   }
 });
 
+function observeButton() {
+  const targetNode = getTargetNode();
+
+  if (targetNode) {
+    checkButtonStatus();
+
+    const config = { attributes: true, childList: false, subtree: false };
+    const callback = function (mutationsList, observer) {
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+          checkButtonStatus();
+        }
+      }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+    observeButtonRetryCount = 0;
+  } else {
+    if (observeButtonRetryCount < 60) {
+      observeButtonRetryCount++;
+      setTimeout(() => {
+        observeButton();
+      }, 1000);
+    } else {
+      console.error("Failed to find targetNode after 60 retries");
+    }
+  }
+}
+
 function clickButton() {
-  const xpath = '//*[@id="MenuBar"]/div[2]/button[1]';
-  const button = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  const button = getTargetNode();
   if (button) {
     button.click();
   }
 }
 
-function observeButton() {
-  const xpath = '//*[@id="MenuBar"]/div[2]/button[1]';
-  const targetNode = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
+function checkButtonStatus() {
+  const targetNode = getTargetNode();
   if (targetNode) {
-    const config = { attributes: true, childList: false, subtree: false };
-    const callback = function (mutationsList, observer) {
-      for (const mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-          const isOn = targetNode.getAttribute('data-status') === 'true';
-          chrome.runtime.sendMessage({ message: 'update_icon', isOn: isOn, disabled: false });
-        }
-      }
-    };
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
+    const isOn = targetNode.getAttribute('data-status') === 'true';
+    sendMessageToBackground({ message: 'update_icon', isOn: isOn, disabled: false });
   } else {
-    chrome.runtime.sendMessage({ message: 'update_icon', isOn: false, disabled: true });
+    sendMessageToBackground({ message: 'update_icon', isOn: false, disabled: true });
   }
 }
 
-function observePage() {
-  const targetNode = document.documentElement;
-
-  if (targetNode) {
-    const config = { childList: true, subtree: true };
-    const callback = function (mutationsList, observer) {
-      for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          observeButton();
-        }
-      }
-    };
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-  }
+function getTargetNode() {
+  const xpath = '//*[@id="MenuBar"]/div[2]/button[1]';
+  return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
 function sendMessageToBackground(message) {
   if (chrome.runtime) {
     chrome.runtime.sendMessage(message);
+  } else {
+    console.error('Extension context invalidated.');
   }
 }
 
-observePage();
-observeButton();
+if (window.location.href.startsWith('https://***.ovice.in/')) {
+  observeButton();
+  checkButtonStatus();
+  window.addEventListener('focus', () => {
+    checkButtonStatus();
+  });
+}
