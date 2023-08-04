8b8f64e8fc956b39dc9a75f5a0d204936dc64fab
n.iwasaki <n.iwasaki>
2023-08-04 22:34:36 +0900

    background.js のリファクタリング

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,151 +1,189 @@
-let globalIconState = {
-  isOn: false,
-  isDisabled: false,
-}
+class OviceTabManager {
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    requestCheckButton().then();
+  async getOveceUrl() {
+    const spaceDomain = await this.getSpaceDomain();
+    return `https://${spaceDomain}.ovice.in/`;
   }
-});
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  getOveceUrl().then((oviceUrl) => {
-    chrome.tabs.get(activeInfo.tabId, (tab) => {
-      if (!isOviceTab(tab, oviceUrl)) {
-        updateExtensionIconImpl(activeInfo.tabId, globalIconState);
-      }
-    });
-  });
-});
-
-chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
-  requestCheckButton();
-});
-
-chrome.action.onClicked.addListener(async () => {
-  sendClickButtonMessage();
-});
-
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === 'install') {
-    chrome.tabs.create({ url: '../html/options.html' });
-  } else if (details.reason === 'update') {
-    reloadOviceTabs();
-  }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'update_icon') {
-    updateExtensionIcon(request.isOn, request.disabled);
-  } else if (request.message === 'reload_ovice_tabs') {
-    reloadOviceTabs();
-  }
-});
-
-function isOviceTab(tab, oviceUrl) {
-  let discarded = tab.discarded;
-  let isOvice = tab.url.startsWith(oviceUrl);
-  return !discarded && isOvice;
-}
-
-function sendCheckButtonMessage() {
-  sendMessageToOviceTab({ message: "check_button" });
-}
-
-function sendClickButtonMessage() {
-  sendMessageToOviceTab({ message: "click_microphone_button" });
-}
-
-function sendMessageToOviceTab(message) {
-  getOveceUrl().then((oviceUrl) => {
-    chrome.tabs.query({}, function (tabs) {
-      for (const tab of tabs) {
-        if (isOviceTab(tab, oviceUrl)) {
-          chrome.tabs.sendMessage(tab.id, message, (response) => {
-            if (chrome.runtime.lastError) {
-              console.info(chrome.runtime.lastError.message);
-            }
-          });
-          return;
+  async getSpaceDomain() {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get("spaceDomain", (data) => {
+        if (data.spaceDomain) {
+          resolve(data.spaceDomain);
+        } else {
+          resolve(undefined);
         }
-      }
+      });
     });
-  });
-}
-
-async function requestCheckButton() {
-  const hasOvice = await hasOviceTab();
-  if (hasOvice) {
-    sendCheckButtonMessage();
-  } else {
-    updateExtensionIcon(false, true);
   }
-}
 
-function updateExtensionIcon(isOn, isDisabled) {
-  globalIconState.isOn = isOn;
-  globalIconState.isDisabled = isDisabled;
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    if (activeTab) {
-      updateExtensionIconImpl(activeTab.id, globalIconState)
-    }
-  });
-}
-
-function updateExtensionIconImpl(tabId, iconState) {
-  const iconPath = iconState.isDisabled
-    ? "../image/icon_disable.png"
-    : iconState.isOn
-    ? "../image/icon_on_green.png"
-    : "../image/icon_off.png";
-
-  chrome.action.setIcon({ tabId: tabId, path: iconPath });
-}
-
-async function hasOviceTab() {
-  return new Promise((resolve) => {
-    getOveceUrl().then((oviceUrl) => {
+  async reloadOviceTabs() {
+    this.getOveceUrl().then((oviceUrl) => {
       chrome.tabs.query({}, (tabs) => {
         for (const tab of tabs) {
-          if (isOviceTab(tab, oviceUrl)) {
-            resolve(true);
+          if (this.isOviceTab(tab, oviceUrl)) {
+            chrome.tabs.reload(tab.id);
+          }
+        }
+      });
+    });
+  }
+
+  async hasOviceTab() {
+    return new Promise((resolve) => {
+      this.getOveceUrl().then((oviceUrl) => {
+        chrome.tabs.query({}, (tabs) => {
+          for (const tab of tabs) {
+            if (this.isOviceTab(tab, oviceUrl)) {
+              resolve(true);
+              return;
+            }
+          }
+          resolve(false);
+        });
+      });
+    });
+  }
+
+  isOviceTab(tab, oviceUrl) {
+    let discarded = tab.discarded;
+    let isOvice = tab.url.startsWith(oviceUrl);
+    return !discarded && isOvice;
+  }
+
+}
+
+class MessageManager {
+
+  constructor(oviceTabManager) {
+    this.oviceTabManager = oviceTabManager;
+  }
+
+  sendCheckButtonMessage() {
+    this.sendMessageToOviceTab({ message: "check_button" });
+  }
+  
+  sendClickButtonMessage() {
+    this.sendMessageToOviceTab({ message: "click_microphone_button" });
+  }
+
+  sendMessageToOviceTab(message) {
+    this.oviceTabManager.getOveceUrl().then((oviceUrl) => {
+      chrome.tabs.query({}, (tabs) => {
+        for (const tab of tabs) {
+          if (this.oviceTabManager.isOviceTab(tab, oviceUrl)) {
+            chrome.tabs.sendMessage(tab.id, message, (response) => {
+              if (chrome.runtime.lastError) {
+                console.info(chrome.runtime.lastError.message);
+              }
+            });
             return;
           }
         }
-        resolve(false);
       });
     });
-  });
+  }
+
 }
 
-async function reloadOviceTabs() {
-  getOveceUrl().then((oviceUrl) => {
-    chrome.tabs.query({}, (tabs) => {
-      for (const tab of tabs) {
-        if (isOviceTab(tab, oviceUrl)) {
-          chrome.tabs.reload(tab.id);
-        }
+class IconManager {
+
+  constructor() {
+    this.iconState = {
+      isOn: false,
+      isDisabled: false,
+    };
+  }
+
+  update(isOn, isDisabled) {
+    this.iconState.isOn = isOn;
+    this.iconState.isDisabled = isDisabled;
+  
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+      if (activeTab) {
+        this.refresh(activeTab.id)
       }
     });
-  });
+  }
+  
+  refresh(tabId) {
+    const iconPath =  this.iconState.isDisabled
+      ? "../image/icon_disable.png"
+      :  this.iconState.isOn
+      ? "../image/icon_on_green.png"
+      : "../image/icon_off.png";
+  
+    chrome.action.setIcon({ tabId: tabId, path: iconPath });
+  }
+
 }
 
-async function getSpaceDomain() {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get("spaceDomain", (data) => {
-      if (data.spaceDomain) {
-        resolve(data.spaceDomain);
-      } else {
-        resolve(undefined);
+class EventListenerManager {
+
+  constructor(oviceTabManager, messageManager, iconManager) {
+    this.oviceTabManager = oviceTabManager;
+    this.messageManager = messageManager;
+    this.iconManager = iconManager;
+  }
+
+  addEventListener() {
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      if (changeInfo.status === "complete") {
+        this.requestCheckButton().then();
       }
     });
-  });
+    
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+      this.oviceTabManager.getOveceUrl().then((oviceUrl) => {
+        chrome.tabs.get(activeInfo.tabId, (tab) => {
+          if (!this.oviceTabManager.isOviceTab(tab, oviceUrl)) {
+            this.iconManager.refresh(activeInfo.tabId);
+          }
+        });
+      });
+    });
+    
+    chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+      this.requestCheckButton();
+    });
+    
+    chrome.action.onClicked.addListener(async () => {
+      this.messageManager.sendClickButtonMessage();
+    });
+    
+    chrome.runtime.onInstalled.addListener((details) => {
+      if (details.reason === 'install') {
+        chrome.tabs.create({ url: '../html/options.html' });
+      } else if (details.reason === 'update') {
+        this.oviceTabManager.reloadOviceTabs();
+      }
+    });
+    
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.message === 'update_icon') {
+        this.iconManager.update(request.isOn, request.disabled);
+      } else if (request.message === 'reload_ovice_tabs') {
+        this.oviceTabManager.reloadOviceTabs();
+      }
+    });
+
+  }
+
+  async requestCheckButton() {
+    const hasOvice = await this.oviceTabManager.hasOviceTab();
+    if (hasOvice) {
+      this.messageManager.sendCheckButtonMessage();
+    } else {
+      this.iconManager.update(false, true);
+    }
+  }
 }
 
-async function getOveceUrl() {
-  const spaceDomain = await getSpaceDomain();
-  return `https://${spaceDomain}.ovice.in/`;
-}
+let oviceTabManager = new OviceTabManager();
+let messageManager = new MessageManager(oviceTabManager);
+let iconManager = new IconManager();
+let eventListenerManager = new EventListenerManager(oviceTabManager, messageManager, iconManager);
+
+eventListenerManager.addEventListener();
